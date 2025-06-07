50b0a916d07bb315e5934969214b8c1d0f842d01
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2025-06-07 15:52:22 +0200

    ExcelLoader one file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,24 +1,47 @@
 package org.example;
 
+import org.example.loader.ExcelLoader;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class App {
-    public final ArrayList<Task> tasks;
-    public final Set<Employee> employees;
+    public ArrayList<Task> tasks;
+    public Set<Employee> employees;
     TerminalInput input;
-    public final Set<Project> projects;
+    public Set<Project> projects;
 //    public final ReportType reportType;
+private final ExcelLoader excelLoader;
+
 
     public App() {
         tasks = new ArrayList<>();
         employees = new HashSet<>();
         projects = new HashSet<>();
+        this.excelLoader = new ExcelLoader();
     }
 
-    public void run() {
+    public void run() throws Exception {
         TerminalInput input = new TerminalInput();
-        input.readFromConsole();
+        this.input = input.readFromConsole();
+        loadDataFromExcel();
     }
+
+    private void loadDataFromExcel() throws Exception {
+
+        excelLoader.setPath(input.getRootPath());
+        ExcelLoader.LoadResult result = excelLoader.loadData();
+
+        this.employees = result.getEmployees();
+        this.projects = result.getProjects();
+        this.tasks = result.getTasks();
+
+        System.out.println("Employees: " + employees.size());
+        System.out.println("Projects: " + projects.size());
+        System.out.println("Tasks: " + tasks.size());
+
+
+    }
+
 }
